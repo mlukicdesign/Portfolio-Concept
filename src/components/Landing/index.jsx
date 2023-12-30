fed839e47a7f2042b33c8d1c213632372eca0124
mlukicdesign <mlukicdesign@gmail.com>
2023-12-30 12:29:30 +0800

    Created Hero section with rolling text

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -1,21 +1,65 @@
 "use client";
 import Image from "next/image";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
 import styles from "./styles.module.scss";
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
+
 
 export default function Header() {
   const firstText = useRef(null);
   const secondText = useRef(null);
   const slider = useRef(null);
+  let xPercent = 0;
+  let direction = -1;
 
+
+
+  useEffect( () => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.to(slider.current, {
+      scrollTrigger: {
+        trigger: document.documentElement,
+        scrub: 0.05, // Adjust the scrub value to slow down the xScroll speed
+        start: 0,
+        end: window.innerHeight,
+        onUpdate: e => direction = e.direction * -1
+      },
+      x: "-100px",
+    })
+    requestAnimationFrame(animate);
+  }, [])
+
+  
+
+//xPercent is set at -100% and gradually icrements to 0%. When it reaches 0% it is reset to -100% and the animation starts again.
+
+  const animate = () => {
+    if(xPercent < -100){
+      xPercent = 0;
+    }
+    else if(xPercent > 0){
+      xPercent = -100;
+    }
+    gsap.set(firstText.current, {xPercent: xPercent})
+    gsap.set(secondText.current, {xPercent: xPercent})
+requestAnimationFrame(animate);
+    xPercent += 0.1 * direction;
+  }
+
+
+  
 
 return (
   <main className={styles.main}>
-    <Image src="/images/background.jpg" fill={true} alt="Background" />
-    <div className={styles.headerContainer}>
+    <Image src="/images/background.jpg" 
+    fill={true} 
+    alt="Background" 
+    />
+    <div className={styles.sliderContainer}>
       <div ref={slider} className={styles.slider}>
-        <div ref={firstText}>Freelance Developer</div>
-        <div ref={secondText}>Freelance Developer</div>
+        <p ref={firstText}>Freelance Developer</p>
+        <p ref={secondText}>Freelance Developer</p>
       </div>
     </div>
   </main>
