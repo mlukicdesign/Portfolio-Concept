9ee15577b1ffd9721fe82ae6c77c41efa3614a77
mlukicdesign <mlukicdesign@gmail.com>
2023-12-31 19:13:16 +0800

    Implenting preloader

--- src/app/page.js
+++ src/app/page.js
@@ -1,36 +1,42 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { AnimatePresence } from "framer-motion";
+import Preloader from "../components/preloader";
 import Landing from "../components/landing";
 import Projects from "../components/projects";
 import Description from "@/components/Description";
 
 export default function Home() {
+
+  const [isLoading, setIsLoading] = useState(true);
+
+
   useEffect(() => {
     (async () => {
       const LocomotiveScroll = (await import("locomotive-scroll")).default;
       const locomotiveScroll = new LocomotiveScroll();
-    })();
+
+      setTimeout(() => {
+        setIsLoading(false);
+        document.body.style.cursor = "default";
+        window.scrollTo(0,0);
+  
+      }, 2000 )
+
+    }
+    )();
   }, []);
 
   return (
     <main>
+
+      <AnimatePresence mode='wait'>
+        {isLoading && <Preloader />}
+      </AnimatePresence>
       <Landing />
       <Description />
       <Projects />
-      {/* <AnimatePresence mode='wait'>
 
-        {isLoading && <Preloader />}
-
-      </AnimatePresence>
-
-
-      <Description />
-
-      <Projects />
-
-      <SlidingImages />
-
-      <Contact /> */}
     </main>
   );
 }
